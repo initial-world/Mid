05dfd54322b63e352ce450da37e73722cb226648
cx <457838017@qq.com>
2020-05-13 20:04:08 +0800

    05

--- 01-Framework/src/main/java/com/example/demo/Application.java
+++ 01-Framework/src/main/java/com/example/demo/Application.java
@@ -1,15 +1,28 @@
 package com.example.demo;
 
+import com.example.demo.domain.User;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 @MapperScan(value = "com.example.demo.dao")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+//        LocalTime localTime = LocalTime.of(1,1,1);
+//        System.out.println(localTime);
+//        System.out.println(localTime.getHour());
+//        System.out.println(localTime.getMinute());
+//        System.out.println(localTime.getSecond());
+ String a = "-10.01";
+        System.out.println(Double.valueOf(a).longValue());
+        System.out.println();
     }
 }
---
+++ 05-multiModule/facade/provider/src/main/java/cn.cx.interfaces/MultiModuleApplication.java
@@ -0,0 +1,10 @@
+package cn.cx.interfaces;
+
+/**
+ * @Descrption TODO
+ * @Author chenxin
+ * @Date 2020/5/13
+ * @Time 19:49
+ */
+public class MultiModuleApplication {
+}
